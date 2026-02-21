0699616d9bc70b7948bdd88417c04d24b478186c
Kevin <iodesme@gmail.com>
2026-02-22 03:29:04 +0900

    Apply localization bindings to all UI files

--- GestureWheel/Pages/AboutPage.xaml.cs
+++ GestureWheel/Pages/AboutPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.Windows;
+using GestureWheel.Localization;
 using GestureWheel.Supports;
 using GestureWheel.Utilities;
 using Serilog;
@@ -25,12 +26,16 @@ namespace GestureWheel.Pages
             try
             {
                 if (!await UpdateSupport.CheckUpdateAsync())
-                    MessageBox.Show("이미 최신 버전을 사용하고 있습니다.", "업데이트 없음", MessageBoxButton.OK, MessageBoxImage.Information);
+                {
+                    var loc = LocalizationManager.Instance;
+                    MessageBox.Show(loc["MsgAlreadyLatest"], loc["MsgNoUpdate"], MessageBoxButton.OK, MessageBoxImage.Information);
+                }
             }
             catch (Exception ex)
             {
                 Log.Error(ex, "An error occurred during manual update checking!");
-                MessageBox.Show("업데이트 확인을 실패했습니다.\n잠시 후 다시 시도해주세요.", "업데이트 확인 실패", MessageBoxButton.OK, MessageBoxImage.Warning);
+                var loc = LocalizationManager.Instance;
+                MessageBox.Show(loc["MsgUpdateFailed"], loc["MsgUpdateFailedTitle"], MessageBoxButton.OK, MessageBoxImage.Warning);
             }
             finally
             {
